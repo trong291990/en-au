868df5ff02eb4154c7fdf86bf119757c826f40aa
trongtd <trongtd@bap.jp>
2024-06-22 10:14:04 +0700

    flashcard

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <!-- Loading Overlay -->
+  <div v-if="loading" class="fixed inset-0 bg-gray-800 bg-opacity-50 z-50 flex items-start justify-center pt-12">
+    <div class="absolute top-[50px] flex items-center space-x-2">
+      <svg class="animate-spin h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+      </svg>
+      <span class="text-white text-lg">Loading...</span>
+    </div>
+  </div>
   <section class="text-gray-600 body-font">
     <div class="text-center pt-4">
       <label class="relative inline-flex items-center cursor-pointer">
