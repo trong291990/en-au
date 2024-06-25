71aad7603dc19f71a3fda16cfce211bc36b63703
trongtd <trongtd@bap.jp>
2024-06-25 20:09:47 +0700

    fc

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Assimil English</title>
+    <title>Flashcard</title>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link type="image/png" sizes="16x16" rel="icon" href="/favicon.png">
 </head>
